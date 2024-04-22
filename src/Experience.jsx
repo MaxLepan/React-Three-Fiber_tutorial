5c8c31c1c892dbb9fc04f66fa6dcff9d5a7d1187
MaxLepan <max.lepan@laposte.net>
2024-04-22 23:21:11 +0200

    Creating and handling meshes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,20 @@
 export default function Experience() {
 
-    
+
 
     return (
         <>
-            <mesh>
-                <torusKnotGeometry/>
-                <meshNormalMaterial />
+            <mesh position-x={ -5 }>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+            <mesh scale={ 10 } rotation-x={ -Math.PI * 0.5 } position-y={ -1 }>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
+            </mesh>
+            <mesh scale={ 1.5 } position-x={ 5 } rotation-y={ Math.PI * .53 }>
+                <boxGeometry scale={ 1.5 } />
+                <meshBasicMaterial color="mediumpurple" />
             </mesh>
         </>
     )
